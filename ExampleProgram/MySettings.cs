ff20a052d89bdef1e6994886652ac3443b5aff59
Jeppe Beier <jeppevinkel@gmail.com>
2023-03-19 23:19:56 +0100

    Initial functionality

--- ExampleProgram/MySettings.cs
+++ ExampleProgram/MySettings.cs
@@ -1,6 +1,11 @@
-﻿namespace ExampleProgram;
+﻿using SettingsManager;
 
-public class MySettings
+namespace ExampleProgram;
+
+public class MySettings : Setting<MySettings>
 {
-    
+    public string Username { get; set; } = "DefaultName";
+    public bool IsRegistered { get; set; } = false;
+    public float SomeValue { get; set; } = 0;
+    public List<int> Numbers { get; set; } = new();
 }
--- SettingsManager/Setting.cs
+++ SettingsManager/Setting.cs
@@ -1,7 +1,54 @@
-﻿namespace SettingsManager
+﻿using System;
+using System.IO;
+
+namespace SettingsManager
 {
-    public class BaseSetting
+    public abstract class Setting<T> where T : Setting<T>, new()
     {
+        private static readonly string FilePath = SettingsManager.GetLocalFilePath($"{typeof(T).Name}.json");
+
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    Load();
+                }
+
+                return _instance ?? throw new InvalidOperationException("Something probably went horribly wrong if this error is thrown.");
+            }
+        }
+
+        public static void Load()
+        {
+            var fileExists = File.Exists(FilePath);
         
+            _instance = (fileExists
+                ? System.Text.Json.JsonSerializer.Deserialize<T>(File.ReadAllText(FilePath))
+                : new T()) ?? new T();
+
+            if (!fileExists)
+            {
+                Save();
+            }
+        }
+
+        public static void Save()
+        {
+            var json = System.Text.Json.JsonSerializer.Serialize(Instance);
+
+            var directoryName = Path.GetDirectoryName(FilePath);
+
+            if (directoryName is null)
+            {
+                throw new InvalidOperationException("directoryName is null");
+            }
+            
+            Directory.CreateDirectory(directoryName);
+            File.WriteAllText(FilePath, json);
+        }
     }
 }
